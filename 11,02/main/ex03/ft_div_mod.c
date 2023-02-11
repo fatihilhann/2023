2c93aefa549282c710b40b4a2c2903dffd7bc1fd
fatihilhann <115029221+fatihilhann@users.noreply.github.com>
2023-02-11 17:21:15 +0300

    Add files via upload

---
+++ 11,02/main/ex03/ft_div_mod.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_div_mod.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: failhan <42istanbul.com.tr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/11 11:48:52 by failhan           #+#    #+#             */
+/*   Updated: 2023/02/11 11:51:46 by failhan          ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void    ft_div_mod(int a, int b, int *div, int *mod)
+{
+    *div = a / b;
+    *mod = a % b;
+}
+
+int        main(void)
+{
+    int a;
+    int b;
+    int div;
+    int mod;
+
+    a = 5;
+    b = 2;
+    ft_div_mod(a, b, &div, &mod);
+    printf("%d / %d = %d, left %d\n", a, b, div, mod);
+}
